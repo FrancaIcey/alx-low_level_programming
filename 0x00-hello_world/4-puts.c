3d609b8b404d928a3b3c5ed7fc3eaa247d7d56a6
Francaicey <francawhyte447@gmail.com>
2023-04-20 15:13:10 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * main -prints a given phrase
+ * Return: Always 0 if success, non zero otherwise
+ */
+int main(void)
+{
+puts("\"Programming is like building a multlingual puzzle");
+return (0);
+}
