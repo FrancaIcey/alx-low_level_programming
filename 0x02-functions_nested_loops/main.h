7eb404ff2c400da887a5a96a9ba92a72f6547f6d
Francaicey <francawhyte447@gmail.com>
2023-04-25 13:32:21 -0700

    task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char);
 void print_alphabet(void);
@@ -13,5 +13,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_times_table(int n);
-
+void print_to_98(int n);
 #endif
