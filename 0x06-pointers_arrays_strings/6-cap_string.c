476b57253623f62ae7878fce3a6fedd3a5105e3d
Francaicey <francawhyte447@gmail.com>
2023-05-04 16:45:55 -0700

    6 Task

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,7 +14,7 @@ char *cap_string(char *str)
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
-		if (str[index - 1] == '' ||
+		if (str[index - 1] == 'a' ||
 			str[index - 1] == '\t' ||
 			str[index - 1] == '\n' ||
 			str[index - 1] == ',' ||
