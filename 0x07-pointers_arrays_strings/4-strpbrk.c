7730bda7960673661258b01d3760f3da45117ff9
Francaicey <francawhyte447@gmail.com>
2023-05-08 15:58:04 -0700

    4 Task

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,18 +10,17 @@ char *_strpbrk(char *s, char *accept)
 {
 
 		int k;
+
 		while (*s)
 		{
 			for (k = 0; accept[k]; k++)
 			{
 			if (*s == accept[k])
-
 			return (s);
 
 			}
 
 		s++;
-
 		}
 
 	return ('\0');
