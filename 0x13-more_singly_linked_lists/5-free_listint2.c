a6184be7a73ceaac8f6b294d4ff3db48d272336f
‘Francaicey’ <francawhyte447@gmail.com>
2023-06-06 16:36:15 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ *free_listint2 - A function that frees a linked list
+ *@head: head
+ *
+ *Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *cursor;
+	listint_t **temp = head;
+
+	if (temp != NULL)
+	{
+		while (*head != NULL)
+		{
+			cursor = *head;
+			free(cursor);
+			*head = (*head)->next;
+		}
+
+		*temp = NULL;
+	}
+}
