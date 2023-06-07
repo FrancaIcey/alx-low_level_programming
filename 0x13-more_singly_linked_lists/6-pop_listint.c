a210c4fecb752242abe5754783f35bb637385857
‘Francaicey’ <francawhyte447@gmail.com>
2023-06-06 17:19:37 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * free_listint2 - A function to free a listint_t list,sets head to NULL
+ * free_listint2 - A function that frees a listint_t list,sets head to NULL
  * @head: pointer listint_t list to freed list
  */
 void free_listint2(listint_t **head)
